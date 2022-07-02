eccc9f5a09bbddd15588596faa1b4347be588d52
MrWakeCZ <joseff.kral@gmail.com>
2022-07-02 22:45:40 +0200

    Fix: AsyncPlayerLevelUpEvent logy

--- src/main/java/cz/craftmania/logger/listeners/external/EconomyLevelUpListener.kt
+++ src/main/java/cz/craftmania/logger/listeners/external/EconomyLevelUpListener.kt
@@ -1,6 +1,6 @@
 package cz.craftmania.logger.listeners.external
 
-import cz.craftmania.crafteconomy.events.PlayerLevelUpEvent
+import cz.craftmania.craftactions.economy.AsyncPlayerLevelUpEvent
 import cz.craftmania.logger.Main
 import cz.craftmania.logger.objects.LogType
 import cz.craftmania.logger.utils.Log
@@ -10,9 +10,9 @@ import org.bukkit.event.Listener
 class EconomyLevelUpListener : Listener {
 
     @EventHandler
-    fun onPlayerLevelUp(event: PlayerLevelUpEvent) {
-        val player = event.player.player // ?????
-        Main.instance!!.sQL!!.createDataLog(player, LogType.LEVELS, "LEVEL_UP", event.newLevel, System.currentTimeMillis())
-        Log.debug("[A:LEVEL_UP]: " + player.name + "(" + player.uniqueId.toString() + ") -> " + event.amount)
+    fun onPlayerLevelUp(event: AsyncPlayerLevelUpEvent) {
+        val player = event.player.player!! // ?????
+        Main.instance!!.sQL!!.createDataLog(player, LogType.LEVELS, "LEVEL_UP", event.newLevel.toLong(), System.currentTimeMillis())
+        Log.debug("[A:LEVEL_UP]: " + player.name + "(" + player.uniqueId.toString() + ") -> " + event.newLevel)
     }
 }
